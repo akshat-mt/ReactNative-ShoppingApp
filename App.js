4a15ff7d2b6dc6e058350bf3864822b62be3a237
Akshat Mani Tripathi <akshatmani1010@gmail.com>
2022-02-13 13:03:11 +0530

    added navigation

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore , combineReducers} from 'redux';
+import { Provider } from "react-redux"
 
+import ProductsReducer from "./store/reducers/prodcuts";
+import Shopnavigator from './navigator/shopnavigator';
+const rootReducer = combineReducers({
+  products: ProductsReducer
+});
+
+const store = createStore(rootReducer);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+       <Shopnavigator />
+    </Provider>
   );
 }
 
